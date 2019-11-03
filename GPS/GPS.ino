1874725ef8c434f60b5f1a75e8c67d9ad6b7b5d8
Tonino <fazioa@gmail.com>
2019-11-03 09:38:46 +0100

    test TRACCAR Client

--- GPS/GPS.ino
+++ GPS/GPS.ino
@@ -1,7 +1,7 @@
 // the setup function runs once when you press reset or power the board
 #include <ESP8266WiFi.h>
 #include "credenziali.h"
-#include <TinyGPS.h>                                 // Tiny GPS Library
+#include <TinyGPS++.h>                                 // Tiny GPS Library
 #include <SoftwareSerial.h>                             // Software Serial Library so we can use other Pins for communication with the GPS module
 
 
@@ -13,6 +13,10 @@ static const int RXPin = 0, TXPin = 2;                // Ublox 6m GPS module to 
 static const uint32_t GPSBaud = 9600;                   // Ublox GPS default Baud Rate is 9600
 
 
+char isotime[24];
+char data[128];
+
+
 //OpenGTS server
 const char* server = "40.112.128.183";
 
@@ -22,11 +26,7 @@ WiFiClient client;
 //Some variable
 int TRACCAR_DEV_ID = ESP.getChipId();
 SoftwareSerial ss(RXPin, TXPin);                        // The serial connection to the GPS device
-TinyGPSPlus gps;                                        // Create an Instance of the TinyGPS++ object called gps
-
- // generate ISO time string
-  char isotime[24];
-  char lastisotime[24];
+TinyGPSPlus  gps;                                        // Create an Instance of the TinyGPS++ object called gps
 
 void setup() {
   Serial.begin(9600);
@@ -43,39 +43,60 @@ void setup() {
   Serial.println("WIFI CONNECTED");
 
   ss.begin(GPSBaud);                                    // Set Software Serial Comm Speed to 9600
-
-
 }
 
 
 
 // the loop function runs over and over again forever
 void loop() {
+  static unsigned long lastutc = 0;
 
-  smartDelay(1500);                                      // Run Procedure smartDelay
-  if (millis() > 5000 && gps.charsProcessed() < 10)
-    Serial.println(F("No GPS data received: check wiring"));
+  smartDelay(3000);                                      // Run Procedure smartDelay
+
+// check WiFi connection and connect to Traccar server if disconnected
+    if (!client.connected()) {
+      Serial.print("Connecting to ");
+      Serial.print(TRACCAR_HOST);
+      Serial.print("...");
+      if (client.connect(TRACCAR_HOST, TRACCAR_PORT)) {
+        Serial.println("OK");
+      } else {
+        Serial.println("failed");
+        delay(3000);
+        return;
+      }
+    }
+  
 
 
-// now that new GPS coordinates are available
+  // check UTC timestamp from GPS
+  //  unsigned long utcdate, utctime;
+  //  gps.get_datetime(&utcdate, &utctime, 0);
+  //  if (utctime == lastutc) {
+  //    return;
+  //  }
+
+  // turn on  indicator LED
+  digitalWrite(BUILTIN_LED, HIGH);
+
+  // now that new GPS coordinates are available
   double lat, lng;
-  lat=gps.location.lat();
-  lng=gps.location.lng();
+  lat = gps.location.lat();
+  lng = gps.location.lng();
   double speed = gps.speed.kmph();
   double alt = gps.altitude.meters();
   int sats = gps.satellites.value();
   double heading = gps.course.deg();
 
   Serial.print("Latitude  : ");
-  Serial.println(lat);
+  Serial.println(lat, 6);
   Serial.print("Longitude : ");
-  Serial.println(lng);
+  Serial.println(lng, 6);
   Serial.print("Satellites: ");
   Serial.println(sats);
   Serial.print("Elevation : ");
   Serial.print(alt);
   Serial.println("m");
-  Serial.print("Time UTC  : ");
   printGPSDateTime();
   Serial.println("");
 
@@ -85,34 +106,42 @@ void loop() {
   Serial.println(gps.speed.mph());
 
 
-  Serial.println("");
-  Serial.println("");
-  //digitalWrite(BUILTIN_LED, HIGH);
-  //digitalWrite(BUILTIN_LED, LOW);
 
-
-
-
-
-
+  // generate ISO time string
   sprintf(isotime, "%04u-%02u-%02uT%02u:%02u:%02u.%01uZ",
-    gps.date.year(), gps.date.month(), gps.date.day(),
-   gps.time.hour(),gps.time.minute(), gps.time.second(), gps.time.centisecond());
+          gps.date.year(), gps.date.month(), gps.date.day(),
+          gps.time.hour(), gps.time.minute(), gps.time.second(), gps.time.centisecond());
+
+  Serial.print("isotime: ");
+  Serial.println(isotime);
 
   // arrange and send data in OsmAnd protocol
   // refer to https://www.traccar.org/osmand
-  char data[128]="&lat=" & lat & "&lon=" & lng & "&altitude=" & alt  & "&speed="  & speed & "&heading=" & heading;
-  
-   // send data
-  client.print(String("GET /?id=") + TRACCAR_DEV_ID + "&timestamp=" + isotime + data + " HTTP/1.1\r\n" +
-    "Host: " + TRACCAR_HOST + "\r\n" + 
-    "Connection: keep-alive\r\n\r\n");
+  String data = "&lat=" + String(lat, 6) + "&lon=" + String(lng, 6)   + "&altitude=" + String(alt, 1) + "&speed=" + String(speed, 1) + "&heading=" + String(heading, 1);
 
-      Serial.println(String("GET /?id=") + TRACCAR_DEV_ID + "&timestamp=" + isotime + data + " HTTP/1.1\r\n" +
-    "Host: " + TRACCAR_HOST + "\r\n" + 
-    "Connection: keep-alive\r\n\r\n");
+  Serial.print("data: ");
+  Serial.println(data);
+
+  Serial.println("");
+  Serial.println("");
+
+
+String URL= "GET /?id=" + String(TRACCAR_DEV_ID) + "&timestamp=" + isotime + data + " HTTP/1.1\r\n" +
+               "Host: " + TRACCAR_HOST + "\r\n" +
+               "Connection: keep-alive\r\n\r\n";
+  // send data
+  client.print(URL);
+
+  // output server response
+  while (client.available()) {
+    Serial.print((char)client.read());
+  }
+
+
+
+  //  // turn off indicator LED
+  digitalWrite(BUILTIN_LED, LOW);
 }
-
 
 static void smartDelay(unsigned long ms)                // This custom version of delay() ensures that the gps object is being "fed".
 {
